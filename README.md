a95ee9b370b774d93e88b4ec636791fd60916556
Michael Mano <michael.mano26@gmail.com>
2018-03-02 16:18:54 +1000

    Updates folder structure

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 Starter Theme
 =============
-
--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * This theme relies apon the Timber WordPress plugin.
+ * 
+ * https://timber.github.io/docs/
+ */
+
 if (!class_exists('Timber')) {
     add_action('admin_notices', function () {
         echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
@@ -12,6 +18,11 @@ if (!class_exists('Timber')) {
     return;
 }
 
-include_once 'core/timber.php';
-include_once 'core/acf.php';
-include_once 'core/actions.php';
+Timber::$dirname = [
+    'views',
+    'views/layouts',
+    'views/partials',
+    'views/macros'
+];
+
+include_once 'core/theme.php';
